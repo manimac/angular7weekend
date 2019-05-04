983fbca1e2fdb2b6bddd9e8abc888363e9e286c7
manimac <manimaccse@gmail.com>
2019-05-04 10:50:32 +0530

    basic commands

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
   providers: []
 })
 export class AppComponent {
-  title = 'angulartraining';
+  title = 'my application';
+  name = 'Dinesh';
+  showClass = true;
+  styleProp = {
+    background : 'red',
+    color : 'green'
+  }
+  save(){
+    this.showClass = !this.showClass;
+  }
+  students = [
+    {
+      name : 'Dinesh',
+      age : 20
+    },
+    {
+      name : 'Karthick',
+      age : 22
+    },
+    {
+      name : 'Raju',
+      age : 27
+    }
+  ]
+
+
 }
---
+++ src/app/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-page',
+  template: '<div>Home component</div>'
+})
+export class HomeComponent {
+  
+}
